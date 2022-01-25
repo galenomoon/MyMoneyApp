ee0e774ed853dde22b7dbb5f7eba9fccf0c98807
galeno <gui.galenocover@gmail.com>
2022-01-25 16:28:50 -0300

    Settings | Server with Express

---
+++ backend/src/config/server.js
@@ -0,0 +1,15 @@
+const port = 3003; //port of backend
+const bodyParser = require("body-parser"); //Middleware - request handling chain
+const express = require("express");
+const server = express(); //Method creating a new server with express
+
+// For every incoming request USE the bodyParser to interpret when it arrives urlencoded
+server.use(bodyParser.urlencoded({ extended: true }));
+//For every incoming request USE the bodyParser to interpret when it arrives as a json
+server.use(bodyParser.json());
+
+//stay listening the port 3003 and if its done.. () => {}
+server.listen(port, function () {
+  console.log(`BACKEND is running on port ${port}
+  ~ Sucesso meu Consagrado :D `);
+});
---
+++ backend/src/loader.js
@@ -0,0 +1,2 @@
+//loader.js is who will be called when my application is loaded
+require('./config/server') //Application Server
