b8e26edefaa6c7c87f72a4cfffdde59adf5b0af5
galeno <gui.galenocover@gmail.com>
2022-01-26 16:23:43 -0300

    Components Menu and Footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,16 @@
 import '../common/template/dependencies'
 import React from 'react'
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
-        <Header/>
+        <Header />
+        <SideBar />
+        <div className="content-wrapper">
+            <h1>Content</h1>
+        </div>
+        <Footer></Footer>
     </div>
 )
