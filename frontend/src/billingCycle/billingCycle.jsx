3dda2793770a312a529b83496315e5279b6835c0
galeno <gui.galenocover@gmail.com>
2022-01-26 17:33:27 -0300

    Reducers and Actions of Dashboard

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div><h1>Ciclo de Pagamento</h1></div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    Ciclos de Pagaentos
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
