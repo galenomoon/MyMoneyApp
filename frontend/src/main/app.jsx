03fe1f5f9e56b91251239536d89fd14d4c9c9c74
galeno <gui.galenocover@gmail.com>
2022-01-26 15:53:21 -0300

    BUG Fix | webpack errors

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,8 @@
+import '../common/template/dependencies'
 import React from 'react'
 
 export default props => (
     <div className='wrapper'>
-        <h1>app</h1>
+        <h1>App</h1>
     </div>
 )
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     extensions: ["", ".js", ".jsx"],
     alias: {
       modules: __dirname + "/node_modules",
-      jquery: "modules/admin-lte/plugins/jQuery/jquery-2.2.2.min.js",
+      jquery: "modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js",
+      bootstrap: "modules/admin-lte/bootstrap/js/bootstrap.js",
     },
   },
   plugins: [
@@ -42,7 +43,7 @@ module.exports = {
         loader: ExtractTextPlugin.extract("style-loader", "css-loader"),
       },
       {
-        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|jpg*.*$/,
+        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
         loader: "file",
       },
     ],
