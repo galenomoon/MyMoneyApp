1c175827cab1bd57acd3300f7536e9fb7ca12aaf
galeno <gui.galenocover@gmail.com>
2022-01-28 15:07:25 -0300

    Finished Project

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,11 +5,22 @@ import { bindActionCreators } from 'redux'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+
+    calculatorSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum),
+        }
+    }
+
     render() {
-        const { handleSubmit, readOnly, credits } = this.props
+        const { handleSubmit, readOnly, credits, debts } = this.props
+        const { sumOfCredits, sumOfDebts } = this.calculatorSummary()
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -19,7 +30,11 @@ class BillingCycleForm extends Component {
                         label="Mês" cols="12 4" placeholder="Informe o mês" />
                     <Field name="year" component={LabelAndInput} type='number' readOnly={readOnly}
                         label="Ano" cols="12 4" placeholder="Informe o ano" />
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts}></Summary>
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly}
+                        field='credits' legend='Créditos' />
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                        field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -34,6 +49,9 @@ class BillingCycleForm extends Component {
 BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
 const selector = formValueSelector('billingCycleForm')
 
-const mapStateToProps = state => ({ credits: selector(state, 'credits') })
+const mapStateToProps = state => ({
+    credits: selector(state, 'credits'),
+    debts: selector(state, 'debts'),
+})
 const mapDispatchToProps = (dispatch) => bindActionCreators({ init }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
--- frontend/src/common/template/menuTree.jsx
+++ frontend/src/common/template/menuTree.jsx
@@ -4,7 +4,10 @@ export default props => (
 
     <li className='treeview'>
         <a href>
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i>
+            <span>
+                {props.label}
+            </span>
             <i className="fa fa-angle-left pull-right"></i>
         </a>
         <ul className="treeview-menu">
